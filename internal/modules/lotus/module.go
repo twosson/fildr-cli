382ac6687d073ab93d510ed6eb7e7c0a0c5b5d0f
twosson <twosson@163.com>
2020-07-16 19:31:46 +0800

    add module for lotus

--- internal/modules/lotus/module.go
+++ internal/modules/lotus/module.go
@@ -10,17 +10,18 @@ import (
 var _ module.Module = (*LotusCollectorModule)(nil)
 
 type LotusCollectorModule struct {
+	roles  []string
 	config *config.TomlConfig
 	logger log.Logger
 }
 
 func New(ctx context.Context, config *config.TomlConfig) (*LotusCollectorModule, error) {
-
-	return nil, nil
+	logger := log.From(ctx)
+	return &LotusCollectorModule{config: config, logger: logger}, nil
 }
 
 func (l *LotusCollectorModule) Name() string {
-	panic("implement me")
+	return `lotus-collector`
 }
 
 func (l *LotusCollectorModule) Start() error {
